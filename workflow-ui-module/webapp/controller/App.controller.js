6dccce9fcc545c38d62555d7793814de2f677126
Laxmikanthappa Bestha <laxmi.ksap9295@gmail.com>
2025-02-05 11:08:10 +0000

    intial push

---
+++ workflow-ui-module/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("ordermgtnsp.workflowuimodule.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ workflow-ui-module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ordermgtnsp/workflow-ui-module/test/unit/controller/orderApprovalForm.controller"
+], function () {
+	"use strict";
+});
